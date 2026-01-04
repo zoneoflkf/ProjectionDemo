4fc68a280564d4344b0b1e3e8def9d0cf255674c
LKF <zoneoflkf@qq.com>
2026-01-04 16:18:25 +0800

    Revert IP address in ServerConfig to previous value for local testing

--- remote-projection-client/app/src/main/java/com/lkf/remotecontrol/net/constants/ServerConfig.kt
+++ remote-projection-client/app/src/main/java/com/lkf/remotecontrol/net/constants/ServerConfig.kt
@@ -1,7 +1,6 @@
 package com.lkf.remotecontrol.net.constants
 
 object ServerConfig {
-//    const val IP = "192.168.1.5"
-    const val IP = "111.230.242.236"
+    const val IP = "192.168.1.5"
     var PORT = 7777
 }
